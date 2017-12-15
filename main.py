7866208b59cd987231e185a3560bc66f19d2ad45
Nirabhra Tapaswi <nirabhratapaswi@gmail.com>
2017-12-15 05:42:02 +0530

    Previous counters implemented

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
 print 'Loading libraries...\n'
-import data_mine, json, get_relevant, TextAnalyser, time
+import data_mine, json, get_relevant, TextAnalyser, time # , pickle
+from cStringIO import StringIO
+
+class conversation:
+    def __init__(self, user_argument):
+        self.user_argument = user_argument
+        self.returned_results = []
+        self.counters = []
+        self.tweets = []
+        self.previous_counters = []
+        self.search_query = ''
+
+    def addCounters(self, counters):
+        self.counters = self.counters + counters
+        return
+    def addTweets(self, tweets):
+        self.tweets = tweets
+        return
+    def addPreviousCounters(self, previous_counters):
+        self.previous_counters = previous_counters
+        return
+    def addSearchQuery(self, search_query):
+        self.search_query = search_query
+        return
+    def addReturnedResults(self, returned_results):
+        self.returned_results = self.returned_results + returned_results
+        return
+
+    def printReturnedResults(self):
+        print '\nSearch Results: \n'
+        index = 0
+        for sentence in self.returned_results:
+            index += 1
+            print '\n', str(index), ') ', sentence
+        return
+    def printCounters(self):
+        print 'Counters: \n'
+        if len(self.counters)==0:
+            print '\nProbably you are right! WOW!'
+        index = 0
+        for line in self.counters:
+            index += 1
+            print '\n', str(index), ') ', line
+            if index==3:
+                break
+        return
+    def printTweets(self):
+        print '\nTweets: \n'
+        index = 0
+        for line in self.tweets:
+            index += 1
+            print '\n', str(index), ') ', line
+        return
 
 # Text Analysis
 raw_query = raw_input('Enter argument: ')
+conversation = conversation(raw_query)
 print 'Starting text analysis...\n'
 # search_query_array = extract_info.noun_phrases(raw_query)
 search_query = TextAnalyser.queryGenerator(raw_query, True)
-print 'Search Query: ', isinstance(search_query, str)
 search_query = str(search_query)
+conversation.addSearchQuery(search_query)
 
 # Mining information
 print 'Mining information off the web...\n'
@@ -24,6 +77,7 @@ for data in returned_data["Result"]:
 
 # Storing the mined information
 print 'Done mining, saving changes...\n'
+conversation.addReturnedResults(returned_data["Result"])
 f = open('info.json', 'w')
 json.dump(returned_data["Result"], f)
 f.close()
@@ -31,40 +85,35 @@ f.close()
 # Calculating similarity within texts
 print 'Generating counters...\n'
 f = open('info.json', 'r+')
-json_read = json.load(f)
+sentences = json.load(f)
+f.close()
+f = open('previous_results.json', 'r+')
+conversation.addReturnedResults(json.load(f))
 f.close()
 
-sentences = json_read
-
+# TODO: combine counters before passing it to get_relevant
 google_range = { 'start': 0, 'end': int(round(len(sentences)/2)) }
 twitter_range = { 'start': int(round(len(sentences)/2)), 'end': len(sentences) }
+previous_results_range = { 'start': 0, 'end': len(conversation.returned_results) }
 counters = get_relevant.get_array(sentences, raw_query, google_range)
+counters = counters + get_relevant.get_array(conversation.returned_results, raw_query, previous_results_range)
 tweets = get_relevant.get_array(sentences, raw_query, twitter_range)
+conversation.addCounters(counters)
+conversation.addTweets(tweets)
 
 response_analyzed_string_array = []
-print 'Search results: \n'
-index = 0
-for sentence in sentences:
-    index += 1
-    print '\n', str(index), ') ', sentence
+conversation.printCounters()
 
-print 'Possible counters: \n'
-if len(counters)>=3:
-    print '',
-	# counter_save = counters[0]
-	# counters[0:2] = counters[1:3]
-	# counters[2] = counter_save
-else:
-	print '\nProbably you are right! WOW!'
-index = 0
-for line in counters:
-    index += 1
-    print '\n', str(index), ') ', line
-    if index==3:
-        break
-index = 0
-for line in tweets:
-    index += 1
-    print '\n', str(index), ') ', line
+f = open('previous_results.json', 'w')
+json.dump(counters, f)
+f.close()
 
-print '\n\n Done:)'
+'''
+f = open('conversation.pkl', 'w')
+src = StringIO()
+p = pickle.Pickler(src)
+# pickle.dump(conversation, f, pickle.HIGHEST_PROTOCOL)
+f.close()
+'''
+
+print '\n\nDone:)'
